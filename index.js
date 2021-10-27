f4d8063e347d94409ae5726a503364d53e5826e4
tip14 <alexeykopyev@gmail.com>
2021-10-27 16:03:24 +0200

    phonebook backend step9

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express();
-
+const cors = require('cors')
 const morgan = require('morgan')
 morgan.token('reqbody', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqbody'))
 
