fb239a24685b0ddd8a2c587b98340e090d728a14
tip14 <alexeykopyev@gmail.com>
2021-10-27 16:53:01 +0200

    commit frontend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 morgan.token('reqbody', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqbody'))
 
